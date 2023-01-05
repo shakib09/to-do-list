8f6c16425afed123defa21afe2f0d08b13f36c62
mash-97 <mahimnhd97@gmail.com>
2023-01-06 01:56:50 +0600

    tests

---
+++ assets/js/test.js
@@ -0,0 +1,28 @@
+let tasks = [];
+let total_tasks = 0;
+
+function build_task_object(task_title, task_description, task_time){
+
+  let task_object = {
+    'id': "task_"+(total_tasks+1),
+    'title': task_title,
+    'description': task_description,
+    'time': task_time
+  };
+  total_tasks += 1;
+  return task_object;
+}
+
+function add_task(task_title, task_description, task_time){
+  let task_object = build_task_object(task_title, task_description, task_time);
+  tasks.push(task_object);
+  return task_object;
+}
+
+
+function delete_task_with_id(task_id){
+  tasks = tasks.filter((task_object)=>{
+    return task_object.id != task_id;  
+  });
+}
+
