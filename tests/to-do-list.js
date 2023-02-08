47d6326ff1821bced84045ca0009e6d0e9a1804e
mash <mahimnhd97@gmail.com>
2023-02-08 22:54:37 +0600

    --

--- tests/to-do-list.js
+++ tests/to-do-list.js
@@ -104,14 +104,13 @@ class User{
     let user = User.getUserObjFromLocalStorage(username);
     if(!user) throw Error(`User (${username}) doesn't exist!`);
     user.password = password;
-    user = User.getDecryptedUser(user);
-    
     if(user.encrypted_password == ToDoListUtilities.hashing_crypt(user.password))
       user.authorized = True;
     return user;
   }
   static saveUserObjInLocalStorage(user) {
     user.password = null;                 // strained point
+    user = User.getEncryptedUser(user);
     localStorage.setItem(user.username, JSON.stringify(user));
   }
   static getEncryptedUser(user) {
@@ -128,13 +127,55 @@ class User{
     if(!uuo) throw Error(`Username(${username}) doesn't exist!`);
 
     let puo = JSON.parse(uuo);
-    return new User(puo.username,
+    let user = new User(puo.username,
                     puo.email,
                     puo.password,
                     puo.encrypted_password,
                     puo.tasks
     );
-    
+    return User.getDecryptedUser(user);
+  }
+  addNewTask(task) {
+    let encrypted_task = Task.getEncryptedTask(new_task, this.password);
+    Task.saveTaskInLocalStorage(encrypted_task);
+    this.tasks.push(encrypted_task.id);
+    User.saveUserObjInLocalStorage(this);
+    return task;
+  }
+  updateTask(task_id, title, description, tags, creation_time, reminder_time, status) {
+    if(this.tasks.indexOf(task_id)<0) 
+      throw Error(`Task(${task_id}) for User(${this.username}) doesn't exist`);
+    let task = Task.getTaskFromLocalStorage(task_id);
+    task = Task.getDecryptedTask(task, this.password);
+    task.title = title;
+    task.description = description;
+    task.tags = tags;
+    task.creation_time = creation_time;
+    task.reminder_time = reminder_time;
+    task.status = status;
+    Task.saveTaskInLocalStorage(Task.getEncryptedTask(task, this.password));
+    return task;
+  }
+  deleteTask(task_id) {
+    if(this.tasks.indexOf(task_id)<0) 
+      throw Error(`Task(${task_id}) for User(${this.username}) doesn't exist`);
+    if(localStorage.getItem(task_id)==null)
+      throw Error(`Task(${task_id}) not found in the storage!`);
+    let task = Task.getTaskFromLocalStorage(task_id);
+    task = Task.getDecryptedTask(task, this.password);
+    localStorage.removeItem(task_id);
+    this.tasks = this.tasks.filter((tid)=>tid!=task_id);
+    User.saveUserObjInLocalStorage(this);
+    return task;
+  }
+  updateTaskStatus(task_id, status) {
+    if(this.tasks.indexOf(task_id)<0) 
+      throw Error(`Task(${task_id}) for User(${this.username}) doesn't exist`);
+    let task = Task.getTaskFromLocalStorage(task_id);
+    task = Task.getDecryptedTask(task, this.password);
+    task.status = status;
+    Task.saveTaskInLocalStorage(Task.getEncryptedTask(task, this.password));
+    return task;
   }
 }
 
@@ -166,6 +207,42 @@ class ToDoListManager {
       else console.log(this.authorized_user);
     }
     catch(error){
+      if(unsuccessful_callback) unsuccessful_callback(error);
+      else console.log(error);
+    }
+  }
+  logout(successful_callback, unsuccessful_callback) {
+    if(this.authorized_user) {
+      this.authorized_user = null;
+      if(successful_callback) successful_callback("Logout successfull!");
+    }
+    else {
+      if(unsuccessful_callback) unsuccessful_callback("No user logged in!");
+    }
+  }
+  addNewTask(title, description, tags, creation_time, reminder_time, status, successful_callback, unsuccessful_callback) {
+    try {
+      let new_task = Task.createNewTask(title, description, tags, creation_time, reminder_time, status);
+      if(!new_task) throw Error("Failed to create task!");
+      User.addNewTask(new_task);
+      if(successful_callback) successful_callback(new_task);
+      else console.log(new_task);
+    }
+    catch(error) {
+      if(unsuccessful_callback) unsuccessful_callback(error);
+      else console.log(error);
+    }
+  }
+  updateTask(task_id, title, description, tags, creation_time, reminder_time, status, successful_callback, unsuccessful_callback) {
+    try {
+      if(this.authorized_user.tasks.indexOf(task_id)<0) 
+        throw Error(`Task(${task_id}) for User(${this.authorized_user.username}) doesn't exist`);
+      let task = this.authorized_user.updateTask(task_id, title, description, tags, creation_time, reminder_time, status);
+      if(!task) throw Error("Failed to update task!");
+      if(successful_callback) successful_callback(task);
+      else console.log(task);
+    }
+    catch(error) {
       if(unsuccessful_callback) unsuccessful_callback(error);
       else console.log(error);
     }
